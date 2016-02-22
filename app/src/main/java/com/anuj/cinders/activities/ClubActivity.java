fdb60255fc57a4881b80c8b458eae242ba750808
anujacharya <anujacharya1@gmail.com>
2016-02-21 21:35:22 -0800

    menu and toolbar

--- app/src/main/java/com/anuj/cinders/activities/ClubActivity.java
+++ app/src/main/java/com/anuj/cinders/activities/ClubActivity.java
@@ -2,6 +2,8 @@ package com.anuj.cinders.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.anuj.cinders.R;
 
@@ -11,6 +13,17 @@ public class ClubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_club, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/anuj/cinders/adapters/ClubHomeAdapter.java
+++ app/src/main/java/com/anuj/cinders/adapters/ClubHomeAdapter.java
@@ -4,4 +4,6 @@ package com.anuj.cinders.adapters;
  * Created by anujacharya on 2/21/16.
  */
 public class ClubHomeAdapter {
+
+
 }
